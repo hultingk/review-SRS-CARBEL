24754181d09bf9d2c586f699504df9f5c785a279
hultingk <hultingk@msu.edu>
2025-07-07 13:44:50 -0400

    update model

--- 03_arthropod_models.R
+++ 03_arthropod_models.R
@@ -5,9 +5,11 @@ librarian::shelf(tidyverse, fixest, glmmTMB, emmeans)
 arthropods <- read.csv(file = file.path("data", "L1", "arthropods.csv"))
 
 
+arthropods <- arthropods %>%
+  mutate(block_round = paste(block, sampling_round, sep = "_"))
 
 # pollinator
-m1 <- fepois(pollinator ~ patch_type * edge_type + log_floral_abundance + focal_count + spider + florivore | block + sampling_round, 
+m1 <- fepois(pollinator ~ patch_type * edge_type + log_floral_abundance + focal_count + spider + florivore | block_round, 
              cluster = "plant_ID",
              data = arthropods)
 etable(m1)
@@ -16,12 +18,12 @@ summary(m1)
 pairs(emmeans(m1, ~ patch_type))
 
 
-m1 <- glmmTMB(pollinator ~ patch_type * edge_type + log_floral_abundance + focal_count + spider + florivore + (1|block) + (1|sampling_round), 
+m1b <- glmmTMB(pollinator ~ patch_type * edge_type + log_floral_abundance + focal_count + spider + florivore + (1|block) + (1|sampling_round), 
              family = "poisson",
              data = arthropods)
-summary(m1)
+summary(m1b)
 # pairwise comparisons
-pairs(emmeans(m1, ~ patch_type))
+pairs(emmeans(m1b, ~ patch_type))
 
 
 # florivore 
