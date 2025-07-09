255bc3bcfd46fa5ae54de99121c88f243bb4a49e
hultingk <hultingk@msu.edu>
2025-07-09 09:17:13 -0400

    update models

--- 03_arthropod_models.R
+++ 03_arthropod_models.R
@@ -1,47 +1,19 @@
 # loading libraries
-librarian::shelf(tidyverse, fixest, glmmTMB, emmeans)
+librarian::shelf(tidyverse, glmmTMB, emmeans, DHARMa)
 
 # loading data
 arthropods <- read.csv(file = file.path("data", "L1", "arthropods.csv"))
 
 
-arthropods <- arthropods %>%
-  mutate(block_round = paste(block, sampling_round, sep = "_"))
 
 # pollinator
-m1 <- fepois(pollinator ~ patch_type * edge_type + log_floral_abundance + focal_count + spider + florivore | block_round, 
-             cluster = "plant_ID",
-             data = arthropods)
-etable(m1)
-summary(m1)
-# pairwise comparisons
-pairs(emmeans(m1, ~ patch_type))
-
-
 m1b <- glmmTMB(pollinator ~ patch_type * edge_type + log_floral_abundance + focal_count + spider + florivore + (1|block) + (1|sampling_round), 
              family = "poisson",
              data = arthropods)
 summary(m1b)
+plot(simulateResiduals(m1b))
+
 # pairwise comparisons
-pairs(emmeans(m1b, ~ patch_type))
+pairs(emmeans(m1b, ~ patch_type*edge_type), simple = "patch_type")
 
 
-# florivore 
-m2 <- fepois(florivore ~ patch_type * edge_type + log_floral_abundance + focal_count | block + sampling_round, 
-             cluster = "plant_ID",
-             data = arthropods)
-etable(m2)
-summary(m2)
-# pairwise comparisons
-pairs(emmeans(m2, ~ patch_type))
-
-
-
-# spider 
-m3 <- fepois(spider ~ patch_type * edge_type + log_floral_abundance + focal_count + florivore | block + sampling_round, 
-             cluster = "plant_ID",
-             data = arthropods)
-etable(m3)
-summary(m3)
-# pairwise comparisons
-pairs(emmeans(m3, ~ patch_type))
